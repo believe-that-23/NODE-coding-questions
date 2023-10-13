6e4ce76b6bb481dfd7cdff7bb72321e2f9705c0f
believe-that-23 <pandey.arnav2399@gmail.com>
2023-10-13 17:49:05 +0530

    solution added

---
+++ Friendship_App/src/features/user/user.model.js
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  avatar: {
+    type: String,
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  gender: {
+    type: String,
+    required: true,
+    enum: ["Male", "Female", "Others"]
+  },
+  auth_tokens: [
+    {
+      token: {
+        type: String,
+        required: true
+      }
+    }
+  ]
+
+}, {
+  timestamps: true
+});
+
+const UserModel = mongoose.model('User', userSchema);
+
+export default UserModel;
